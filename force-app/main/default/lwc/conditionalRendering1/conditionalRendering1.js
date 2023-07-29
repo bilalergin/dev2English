6fa35c8368fecf3c0918a31efbc8f99043c8f88d
bilalergin <102329249+bilalergin@users.noreply.github.com>
2023-07-29 06:15:37 +0300

    Conditional Rendering 1

---
+++ force-app/main/default/lwc/conditionalRendering1/conditionalRendering1.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+//! 29.07.2023
+export default class ConditionalRendering1 extends LightningElement {
+    expression1 = true; 
+    expression2 = true; 
+    expression3 = true; 
+}
