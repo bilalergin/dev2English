51ec3b0714c9aeb5636e7b1d1e5917af5b167218
bilalergin <102329249+bilalergin@users.noreply.github.com>
2023-08-18 06:44:48 +0300

    new

--- force-app/main/default/lwc/p2cPrimitivesParent1/p2cPrimitivesParent1.js
+++ force-app/main/default/lwc/p2cPrimitivesParent1/p2cPrimitivesParent1.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class P2cPrimitivesParent1 extends LightningElement {
+//modularity, reusebility, readability her zaman hatırmalamız gereken 3 özellik kod yazarken 
+
     emp = {
         name: 'Bilal',
         age : 36,
