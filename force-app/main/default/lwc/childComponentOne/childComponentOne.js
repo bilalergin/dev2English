17b6f4136584a6d88aaad2ac3298d74563b1cd0b
bilalergin <102329249+bilalergin@users.noreply.github.com>
2023-08-22 04:37:26 +0300

    new

--- force-app/main/default/lwc/childComponentOne/childComponentOne.js
+++ force-app/main/default/lwc/childComponentOne/childComponentOne.js
@@ -6,8 +6,13 @@ export default class ChildComponentOne extends LightningElement {
 
    @api handleChange(){
         if(this.value == 100)
-        this.value=100;
+        this.value=200;
     else
         this.value=100;
     }
 }
+/*1.) Created a child component
+2.) Created a method n child JS and annotated it with @api
+3.) Created a parent component
+4.) Added child component into the Parent component strucuture
+5.) IN Parent comp JS, we use template property to query the parent html and invoke the child js method.*/
--- force-app/main/default/lwc/parentComponentOne/parentComponentOne.js
+++ force-app/main/default/lwc/parentComponentOne/parentComponentOne.js
@@ -2,4 +2,7 @@ import { LightningElement } from 'lwc';
 
 export default class ParentComponentOne extends LightningElement {
 
+    handleClick(){
+        this.template.querySelector("c-child-component-one").handleChange();
+    }
 }
